b843648f2c0db795fe9a052b87896e7f5a807bd3
Kinshuk <kinshuk283641@gmail.com>
2026-05-10 23:03:30 +0530

    final check

--- atm.py
+++ atm.py
@@ -1,8 +1,25 @@
 import database
 
+import os
+from gtts import gTTS
+from playsound import playsound
+
+def speak(text):
+
+    voice = gTTS(
+        text=text,
+        lang="en",
+        slow=False
+    )
+    if os.path.exists("voice.mp3"):
+        os.remove("voice.mp3")
+
+    voice.save("voice.mp3")
+    playsound("voice.mp3")
 
 def checkbalance():
-    print(f"Your balance is ₹{database.balance}")
+    print(f"Your balance is Rupees{database.balance}")
+    speak(f"Your Balance is Rupees{database.balance}")
 
 
 def depositmoney():
@@ -11,7 +28,9 @@ def depositmoney():
     database.balance += amount
 
     print(f"₹{amount} deposited successfully.")
+    speak(f"₹{amount} deposited successfully.")
     print(f"Updated Balance: ₹{database.balance}")
+    speak(f"Updated Balance: ₹{database.balance}")
 
 
 def withdrawmoney():
@@ -21,7 +40,11 @@ def withdrawmoney():
         database.balance -= amount
 
         print(f"₹{amount} withdrawn successfully.")
+        speak(f"₹{amount} withdrawn successfully.")
+
         print(f"Remaining Balance: ₹{database.balance}")
+        speak(f"Remaining Balance: ₹{database.balance}")
 
     else:
-        print("Insufficient balance")
+        print("Get a job")
+        speak("Get a job")
--- database.py
+++ database.py
@@ -1,19 +1,1 @@
-import os
-from gtts import gTTS
-from playsound import playsound
-
-def speak(text):
-
-    voice = gTTS(
-        text=text,
-        lang="en",
-        slow=False
-    )
-    if os.path.exists("voice.mp3"):
-        os.remove("voice.mp3")
-
-    voice.save("voice.mp3")
-    playsound("voice.mp3")
-
 balance = 5000
-speak("Your Current Balance is",balance)
